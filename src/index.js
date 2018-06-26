ee9a977db20240125413eaf5ada0c5e865489e33
Hack Reactor Student <student@hackreactor.com>
2018-06-25 20:00:51 -0400

    made App a class, created state,
    -Passed some of the App tests
    -Working on video player now

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 // TODO: Render the `App` component to the DOM
 
 ReactDOM.render(<App />, document.getElementById('app'));
-// ReactDOM.render(<Search />, document.getElementsByClassName('col-md-5'))
-// ReactDOM.render(<VideoList />, document.getElementsByClassName('col-md-5'))
-// ReactDOM.render(<VideoPlayer />, document.getElementsByClassName('col-md-7'))
 
